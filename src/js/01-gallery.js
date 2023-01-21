f6d599a4dcceea5c7c66e5bfb2100b88f1e057b7
Gotsman Roman <gotsmanroman@gmail.com>
2023-01-21 02:43:41 +0200

    Додано Завдання 1 - Галерея

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,41 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRefs = document.querySelector('.gallery');
+galleryRefs.innerHTML = createGalleryMarkUp();
+
+function createGalleryMarkUp() {
+  return galleryItems
+    .map(elem => {
+      return `
+        <a class="gallery__item" href="${elem.original}">
+          <img
+            class="gallery__image"
+            src="${elem.preview}"npm
+            alt="${elem.description}"
+          />
+      </a>
+      `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  caption: 'true',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {
+  // Do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {
+  console.log(e); // Some usefull information
+});
+
 console.log(galleryItems);
